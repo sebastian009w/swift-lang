ce67d537e468763cb9c641f9e33675e12b1fb450
Sebastian009w <ia3266595@gmail.com>
2023-06-08 10:38:49 -0500

    control de flujo

---
+++ controlFlujo.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+var num = 10
+
+
+if num == 10 && num == 11 {
+    print("Valor number 10")
+}else if num == 8 || num == 12 {
+    print("el valor es 8")
+} else {
+    print("el valor no es ninguno")
+    
+}
