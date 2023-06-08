ba143d02e9cf34e604c264dc11182924c1cd5b1c
Sebastian009w <ia3266595@gmail.com>
2023-06-08 10:52:21 -0500

    bucles

---
+++ for.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+var myList = ["asdas"]
+
+for value in myList {
+    print(value)
+    
+}
---
+++ while.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+
+var myList = ["asdasda"]
+
+while index < myList.count {
+    print(myList[index])    
+    index += 1    
+}
